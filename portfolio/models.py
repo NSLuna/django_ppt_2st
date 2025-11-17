20a6af5b17be40b388a6b97b22ad6a0fb19c87a1
NSLuna <qkrdls250@gmail.com>
2025-11-17 23:11:16 +0900

    PPT Django backend setup

---
+++ portfolio/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class About(models.Model):
+    name = models.CharField(max_length=50)
+    role = models.CharField(max_length=100)
+    description = models.TextField()
+    github = models.CharField(max_length=100, blank=True)
+    blog = models.CharField(max_length=200, blank=True)
+
+    def __str__(self) :
+        return self.name
+    
+
+class Project(models.Model) :
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    tech_stack = models.CharField(max_length=300, blank=True)
+    github = models.CharField(max_length=300, blank=True)
+
+
+    def __str__(self):
+        return self.title
